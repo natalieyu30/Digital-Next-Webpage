c4ebb3cc1629ea18c3d59925d67ea0d2ab1cb727
Natalie Yu <natalieyu30@gmail.com>
2021-01-09 09:40:51 -0500

    add scroll effect functions

--- app.js
+++ app.js
@@ -13,3 +13,37 @@ navLinks.forEach(link => link.addEventListener('click', () => {
   navList.classList.remove('show-list')
   })
 );
+
+// SCROLL - ACTIVE LINK
+const sections = document.querySelectorAll('section');
+console.log(sections);
+
+function scrollActiveLink() {
+  const scrollY = window.pageYOffset;
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop-85;
+    // console.log(sectionTop)
+    const SectionHeight = section.offsetHeight;
+    sectionId = section.getAttribute('id')
+
+  
+    if (scrollY >= sectionTop && scrollY < sectionTop + SectionHeight) {
+      document.querySelector(`#nav-list a[href='#${sectionId}']`).classList.add('active-link');
+    } else {
+      document.querySelector(`#nav-list a[href='#${sectionId}']`).classList.remove('active-link');
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActiveLink);
+
+// SCROLL-TOP BTN
+function scrollTop() {
+  const scrollTop = document.querySelector('.scroll-top');
+  if (this.scrollY >= 560) {
+    scrollTop.classList.add('show-scrollTop');
+  } else {
+    scrollTop.classList.remove('show-scrollTop');
+  }
+}
+window.addEventListener('scroll', scrollTop);
